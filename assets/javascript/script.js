036837264b745386a7b3ad11402dcf6db37adb8e
LinnetteCapul <linnette.capul@gmail.com>
2022-03-04 11:18:29 -0800

    added live time

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,4 +1,8 @@
-
-
 var today = moment();
-$(".live-time").text(today.format("MMM Do, YYYY"));
+$(".live-time").text(today.format("dddd, MMMM Do YYYY, h:mm:ss a"));
+ 
+function time() {
+    var today = moment();
+    $(".live-time").text(today.format("dddd, MMMM Do YYYY, h:mm:ss a"));
+}
+setInterval(time, 1000);
